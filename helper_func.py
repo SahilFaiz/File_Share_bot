065e539b4bd125e68b2a508847b467a39444db97
Sayyed Faizan <103510430+SahilFaiz@users.noreply.github.com>
2024-05-12 23:40:21 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -33,7 +33,7 @@ async def encode(string):
 
 async def decode(base64_string):
     base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
-    base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
+    base64_bytes = base64_string.encode("ascii")
     string_bytes = base64.urlsafe_b64decode(base64_bytes) 
     string = string_bytes.decode("ascii")
     return string
